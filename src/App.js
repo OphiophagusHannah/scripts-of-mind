629d2b6384b6dbc76df4b920d97c1bcf61afb0ce
ophiophagushannah <hannahavgust@gmail.com>
2019-06-20 00:00:32 -0500

    opacity

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Ophiophagus Hannah</h1>
+        <h1>ophiophagus_hannah :: scripts of mind</h1>
         <GetLocalPosts/>
 
       </div>
